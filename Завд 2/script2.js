c78d4b51fc24d3ebae2893c192cb8ba1ed3d0308
Yaroslav Protsenko <opexyash@gmail.com>
2022-05-09 15:14:07 +0300

    bug fixed with img

--- Завд 2/script2.js
+++ Завд 2/script2.js
@@ -1,25 +1,24 @@
-let card =document.querySelectorAll(".card");
-let back =document.querySelectorAll(".back");
-let front =document.querySelectorAll(".front");
+let card = document.querySelectorAll(".card");
+let back = document.querySelectorAll(".back");
+let front = document.querySelectorAll(".front");
 
 let img = document.querySelectorAll('img');
-for(let i of img){
-    i.addEventListener('dragstart', function(event) {
+for (let i of img) {
+    i.addEventListener('dragstart', function (event) {
         event.preventDefault();
     });
 };
 
 
-for(let i=0;i<back.length;i++){
-   back[i].addEventListener('click',function(){
+for (let i = 0; i < back.length; i++) {
+    back[i].addEventListener('click', function () {
         back[i].style.transform = 'rotateX(180deg)';
         front[i].style.transform = 'rotateX(360deg)';
     });
 };
-for(let i=0;i<front.length;i++){
-    front[i].addEventListener('click',function(){
-         back[i].style.transform = 'rotateX(0deg)';
-         front[i].style.transform = 'rotateX(180deg)';
-     });
- };
-
+for (let i = 0; i < front.length; i++) {
+    front[i].addEventListener('click', function () {
+        back[i].style.transform = 'rotateX(0deg)';
+        front[i].style.transform = 'rotateX(180deg)';
+    });
+};
